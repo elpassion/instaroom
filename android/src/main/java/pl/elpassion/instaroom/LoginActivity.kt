beeac43602ff66f39bb59a23e5c6814e38980e00
Paweł Gajda <pawegio@gmail.com>
2018-10-04 17:05:35 +0200

    Add Google Sign-In

--- android/src/main/java/pl/elpassion/instaroom/LoginActivity.kt
+++ android/src/main/java/pl/elpassion/instaroom/LoginActivity.kt
@@ -1,12 +1,55 @@
 package pl.elpassion.instaroom
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast.LENGTH_LONG
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import kotlinx.android.synthetic.main.login_activity.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.server_client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+
+        signInButton.setOnClickListener {
+            startActivityForResult(googleSignInClient.signInIntent, SIGN_IN_REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(task: Task<GoogleSignInAccount>) {
+        try {
+            val result = task.getResult(ApiException::class.java)
+            Log.d(javaClass.simpleName, "Token: ${result?.idToken}")
+            toast("Signed in", duration = LENGTH_LONG).show()
+        } catch (e: ApiException) {
+            e.printStackTrace()
+        }
+    }
+
+    companion object {
+        const val SIGN_IN_REQUEST_CODE = 627
     }
 }
