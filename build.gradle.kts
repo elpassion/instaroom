e5f745d62b8cc0203a108060ea49b00dfd46a390
Paweł Gajda <pawegio@gmail.com>
2018-10-24 00:11:31 +0200

    Convert top-level build file to Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+buildscript { 
+    val kotlinVersion = "1.2.71"
+
+    repositories {
+        google()
+        jcenter()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.android.tools.build:gradle:3.2.0")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven("https://jitpack.io")
+        maven("http://dl.bintray.com/kotlin/ktor")
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
