10d1adfaec10e4bacef61e2a556e4cf8e1313ab3
Marek Langiewicz <marek.langiewicz@gmail.com>
2019-01-11 00:35:46 +0100

    Remove duplications from calendar related code

--- backend/src/Kalendar.kt
+++ backend/src/Kalendar.kt
@@ -3,6 +3,7 @@ package pl.elpassion.instaroom
 import com.google.api.client.auth.oauth2.BearerToken
 import com.google.api.client.auth.oauth2.Credential
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.http.HttpResponseException
 import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.client.util.DateTime
 import com.google.api.services.calendar.Calendar
@@ -10,45 +11,25 @@ import com.google.api.services.calendar.model.EventAttendee
 import com.google.api.services.calendar.model.EventDateTime
 
 enum class Salka(val title: String, val calendarId: String) {
-
-    SALKA_PRZY_DEVELOPERACH("Salka przy deweloperach", idSalkaPrzyDeveloperach),
-    SALKA_PRZY_RECEPCJI("Salka przy recepcji", idSalkaPrzyRecepcji),
-    SALKA_ZIELONA("Salka zielona", idSalkaZielona),
-    SALKA_ZOLTA("Salka zolta", idSalkaZolta),
-    SALKA_PRZY_GRAFIKACH("Salka przy grafikach", idSalkaPrzyGrafikach)
-
+    SALKA_PRZY_DEVELOPERACH("Salka przy deweloperach", "elpassion.pl_2d3431363530383233373435@resource.calendar.google.com"),
+    SALKA_PRZY_RECEPCJI("Salka przy recepcji", "elpassion.pl_3336373234343038393630@resource.calendar.google.com"),
+    SALKA_ZIELONA("Salka zielona", "elpassion.pl_36303736393039313938@resource.calendar.google.com"),
+    SALKA_ZOLTA("Salka zolta", "elpassion.pl_2d3837303539373033363132@resource.calendar.google.com"),
+    SALKA_PRZY_GRAFIKACH("Salka przy grafikach", "elpassion.pl_34313639343833323536@resource.calendar.google.com")
 }
 
 data class Event(val name: String?, val startTime: String, val endTime: String)
 
 data class Room(val name: String?, val calendarId: String, val events: List<Event>)
 
-
-private val idSalkaPrzyDeveloperach = "elpassion.pl_2d3431363530383233373435@resource.calendar.google.com"
-private val idSalkaPrzyRecepcji = "elpassion.pl_3336373234343038393630@resource.calendar.google.com"
-private val idSalkaZielona = "elpassion.pl_36303736393039313938@resource.calendar.google.com"
-private val idSalkaZolta = "elpassion.pl_2d3837303539373033363132@resource.calendar.google.com"
-private val idSalkaPrzyGrafikach = "elpassion.pl_34313639343833323536@resource.calendar.google.com"
-
 private val transport = GoogleNetHttpTransport.newTrustedTransport()
 
 private val jsonFactory = JacksonFactory.getDefaultInstance()
 
 fun calendarStuff(token: String): List<String> {
-
     val service = createCalendarService(token)
-
-    val results = try {
-        listOf("Salka przy developerach") + service.getSomeEventsStrings(idSalkaPrzyDeveloperach) +
-                listOf("Salka przy recepcji") + service.getSomeEventsStrings(idSalkaPrzyRecepcji) +
-                listOf("Salka zielona") + service.getSomeEventsStrings(idSalkaZielona) +
-                listOf("Salka zolta") + service.getSomeEventsStrings(idSalkaZolta) +
-                listOf("Salka przy grafikach") + service.getSomeEventsStrings(idSalkaPrzyGrafikach)
-    } catch (e: Exception) {
-        listOf("Blad dostepu do salek: $e")
-    }
-
-    return results
+    return try { Salka.values().flatMap(service::getSomeEventsStrings) }
+    catch (e: HttpResponseException) { listOf(e.message.orEmpty()) }
 }
 
 fun bookSomeRoom(accessToken: String, calendarId: String) =
@@ -78,16 +59,11 @@ private fun createCalendarService(accessToken: String) =
 
 fun getSomeRooms(accessToken: String) = createCalendarService(accessToken).getSomeRooms()
 
-private fun Calendar.getSomeRooms() = listOf(
-    getRoom("Salka przy deweloperach", idSalkaPrzyDeveloperach),
-    getRoom("Salka przy recepcji", idSalkaPrzyRecepcji),
-    getRoom("Salka zielona", idSalkaZielona),
-    getRoom("Salka zolta", idSalkaZolta),
-    getRoom("Salka przy grafikach", idSalkaPrzyGrafikach)
-)
+private fun Calendar.getSomeRooms() = Salka.values().map(this::getRoom)
 
-private fun Calendar.getRoom(name: String, calendarId: String) =
-    Room(name, calendarId, getSomeEvents(calendarId))
+private fun Calendar.getRoom(salka: Salka) = getRoom(salka.title, salka.calendarId)
+
+private fun Calendar.getRoom(name: String, calendarId: String) = Room(name, calendarId, getSomeEvents(calendarId))
 
 private fun Calendar.getSomeEvents(calendarId: String) =
     events().list(calendarId)
@@ -98,6 +74,8 @@ private fun Calendar.getSomeEvents(calendarId: String) =
         .execute()
         .items
         .map { Event(it.summary, it.start.dateTime.toString(), it.end.dateTime.toString()) }
+
+private fun Calendar.getSomeEventsStrings(salka: Salka) = listOf(salka.title) + getSomeEventsStrings(salka.calendarId)
 
 private fun Calendar.getSomeEventsStrings(calendarId: String) =
     getSomeEvents(calendarId).map { "${it.name} (${it.startTime} - ${it.endTime})" } + listOf("and the calendar id is: $calendarId")
